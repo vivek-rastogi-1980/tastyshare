45b5adcc00c2bb5c7882318588147f7a0ae815ac
vivek-rastogi-1980 <vivek.rastogi.work@gmail.com>
2025-07-27 01:07:47 +0530

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = https://hbupdjyrblcvyswblacz.supabase.co;
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhidXBkanlyYmxjdnlzd2JsYWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNzc3MzIsImV4cCI6MjA2ODk1MzczMn0.4p-FrgpOMd0IcSPZBHuJN52EC7z_jzDoXjo2920Al8s;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
