fc46abe841b95da443e81eebe61e78a89e36f339
vivek-rastogi-1980 <vivek.rastogi.work@gmail.com>
2025-07-28 14:27:53 +0530

    fix: fix supabase client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://hbupdjyrblcvyswblacz.supabase.co;
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhidXBkanlyYmxjdnlzd2JsYWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNzc3MzIsImV4cCI6MjA2ODk1MzczMn0.4p-FrgpOMd0IcSPZBHuJN52EC7z_jzDoXjo2920Al8s;
+const supabaseUrl = "https://hbupdjyrblcvyswblacz.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhidXBkanlyYmxjdnlzd2JsYWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNzc3MzIsImV4cCI6MjA2ODk1MzczMn0.4p-FrgpOMd0IcSPZBHuJN52EC7z_jzDoXjo2920Al8s";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
